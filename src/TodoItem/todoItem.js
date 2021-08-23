e44ea6d31b7131f16a479dcbba1dedc5c4b40500
Ben Smith <Plethora@Steven-Plabicus-II.local>
2021-08-23 14:50:58 +0100

    commit1

---
+++ src/TodoItem/todoItem.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './style.css';
+
+class TodoItem extends React.Component {
+
+    render() {
+
+        const { todo } = this.props;
+
+        return(
+        <div id="todoItem" classnName={'todoItem' + (todo.completed ? ' completed' : '')} onClick={this.toggleTodo}>{todo.text}</div>
+        );
+    }
+
+    toggleTodo = () => {
+      this.props.updateTodoFn(this.props.todo);
+    }
+}
+
+export default TodoItem;
